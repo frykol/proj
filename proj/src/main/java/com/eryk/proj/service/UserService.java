9596bd2d015d3bbafac61fc874917061d993cb0f
frykol <96352817+frykol@users.noreply.github.com>
2022-03-17 08:55:57 +0100

    Added UserController

--- proj/src/main/java/com/eryk/proj/service/UserService.java
+++ proj/src/main/java/com/eryk/proj/service/UserService.java
@@ -6,6 +6,7 @@ import com.eryk.proj.repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Service
@@ -33,6 +34,7 @@ public class UserService {
         return userRepo.save(user);
     }
 
+    @Transactional
     public void deleteUser(Long id){
         userRepo.deleteUserById(id);
     }
