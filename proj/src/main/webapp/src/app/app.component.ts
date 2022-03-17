6163b1ebb0e5a97b58c00545f09f68fc0045cf98
frykol <96352817+frykol@users.noreply.github.com>
2022-03-17 11:42:07 +0100

    http service in angular

--- proj/src/main/webapp/src/app/app.component.ts
+++ proj/src/main/webapp/src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { User } from './user';
+import { UserService } from './user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'webapp';
+export class AppComponent implements OnInit {
+  users: User[] = [];
+
+  constructor(private userService: UserService){}
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  getUsers(): void{
+    this.userService.getUsers().subscribe(
+      (response: User[]) => {this.users = response},
+      (error: HttpErrorResponse) => {alert(error)}
+    )
+  }
+
+  //
+
 }
---
+++ proj/src/main/webapp/src/app/user.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs';
+import { User } from './user';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private apiUrl:string = environment.apiUrl;
+
+  constructor(private http: HttpClient) { }
+
+  public getUsers(): Observable<User[]>{
+    return this.http.get<User[]>(`${this.apiUrl}/user/all`);
+  }
+
+  public addUser(user: User): Observable<User>{
+    return this.http.post<User>(`${this.apiUrl}/user/add`, user);
+  }
+
+  public updateUser(user: User): Observable<User>{
+    return this.http.put<User>(`${this.apiUrl}/user/update`, user);
+  }
+
+  public deleteUser(userId: number): Observable<void>{
+    return this.http.delete<void>(`${this.apiUrl}/user/delete/${userId}`);
+  }
+}
