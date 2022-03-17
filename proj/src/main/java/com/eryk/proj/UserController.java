ea3c61a7e8b9286f66fe20791fb05f113a9d04a6
frykol <96352817+frykol@users.noreply.github.com>
2022-03-17 10:16:39 +0100

    started webapp

--- proj/src/main/java/com/eryk/proj/UserController.java
+++ proj/src/main/java/com/eryk/proj/UserController.java
@@ -2,6 +2,7 @@ package com.eryk.proj;
 
 import com.eryk.proj.model.User;
 import com.eryk.proj.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,8 +12,10 @@ import java.util.List;
 @RestController
 @RequestMapping("/user")
 public class UserController {
+    @Autowired
     private final UserService userService;
 
+    @Autowired
     public UserController(UserService userService) {
         this.userService = userService;
     }
--- proj/src/main/java/com/eryk/proj/model/User.java
+++ proj/src/main/java/com/eryk/proj/model/User.java
@@ -6,14 +6,13 @@ import lombok.Setter;
 import javax.persistence.*;
 import java.io.Serializable;
 
-@Getter
-@Setter
 @Entity
+@Table(name="users")
 public class User implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(nullable = false, updatable = false)
-    private long id;
+    private Long id;
     private String username;
     private String email;
     private String description;
@@ -23,6 +22,38 @@ public class User implements Serializable {
     public User(String username, String email, String description){
         this.username = username;
         this.email = email;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
         this.description = description;
     }
 }
